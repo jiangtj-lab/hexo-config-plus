1960f49dc73867b38a995af90059a531826fc03f
jiangtj <116749895@qq.com>
2020-01-06 10:26:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hexo Config Plus
+[![](https://img.shields.io/npm/v/@jiangtj/hexo-config-plus.svg?style=popout-square)](https://www.npmjs.com/package/@jiangtj/hexo-config-plus)
+![npm](https://img.shields.io/npm/dt/@jiangtj/hexo-config-plus.svg?style=popout-square)
+![npm](https://img.shields.io/npm/l/@jiangtj/hexo-config-plus?style=popout-square)
+
+## Install
+
+```bash
+yarn add @jiangtj/hexo-config-plus
+```
+
+## Use
+
+```yml
+config_plus:
+  theme: next
+  languages: languages
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { mergeWith } = require('lodash');
+
+hexo.on('generateBefore', () => {
+  
+  let config = Object.assign({
+    theme: 'next',
+    languages: 'languages'
+  }, hexo.config.config_plus)
+
+  let merge = (object, sources) => {
+    return mergeWith(object, sources, (objValue, srcValue) => {
+      if (Array.isArray(objValue)) {
+        return srcValue;
+      }
+    });
+  }
+
+  let data = hexo.locals.get('data') || {};
+
+  // Read config from data.next or theme_config
+  merge(hexo.theme.config, data[config.theme]);
+
+  // Custom languages support.
+  if (data[config.languages]) {
+    return;
+  }
+  let mergeLang = (lang) => {
+    i18n.set(lang, merge(i18n.get([lang]), data[config.languages][lang]));
+  };
+  let lang = hexo.config.language;
+  let i18n = hexo.theme.i18n;
+  if (Array.isArray(lang)) {
+    lang.forEach(item => mergeLang(item));
+  } else {
+    mergeLang(lang);
+  }
+});
